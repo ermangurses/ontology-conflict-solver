496d33475e698ffbcf8accc97816595e81463d78
ermangurses <egurses@email.arizona.edu>
2018-03-01 21:43:27 -0700

    Adding Constants Class

---
+++ app/src/main/java/edu/arizona/biosemantics/conflictsolver/Constants.java
@@ -0,0 +1,10 @@
+package edu.arizona.biosemantics.conflictsolver;
+
+
+public class Constants {
+
+    private static final String BASE_URL           = "http://192.168.1.141/Android/dev/";
+    public static final  String URL_REGISTER = BASE_URL + "register.php";
+    public static final  String URL_LOGIN    = BASE_URL + "login.php";
+
+}
