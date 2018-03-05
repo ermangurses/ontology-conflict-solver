a5b8bf24d16c86f3ab09508badb5e3104b1aefd4
ermangurses <egurses@email.arizona.edu>
2018-03-05 15:32:35 -0700

    User registration is functional at client side

--- app/src/main/java/edu/arizona/biosemantics/conflictsolver/MainActivity.java
+++ app/src/main/java/edu/arizona/biosemantics/conflictsolver/MainActivity.java
@@ -1,44 +1,113 @@
 package edu.arizona.biosemantics.conflictsolver;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 
-    private TextView mTextMessage;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+import java.util.HashMap;
+import java.util.Map;
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editTextUsername, editTextEmail, editTextPassword;
+    private Button buttonRegister;
+    private ProgressDialog progressDialog;
+
+    private TextView textViewLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        /*
+        * Check whether the user logged in or not
+        * If so, jump to profile
+        */
+
+        if(SharedPrefManager.getInstance(this).isLoggedIn()){
+            finish();
+            startActivity(new Intent(this, ProfileActivity.class));
+            return;
+        }
+
+        editTextEmail    = (EditText) findViewById(R.id.editTextEmail);
+        editTextUsername = (EditText) findViewById(R.id.editTextUsername);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        textViewLogin    = (TextView) findViewById(R.id.textViewLogin);
+        buttonRegister   = (  Button) findViewById(R.id.buttonRegister);
+        progressDialog   = new ProgressDialog(this);
+
+        buttonRegister.setOnClickListener(this);
+        textViewLogin.setOnClickListener(this);
     }
 
+    private void registerUser() {
+        final String email = editTextEmail.getText().toString().trim();
+        final String username = editTextUsername.getText().toString().trim();
+        final String password = editTextPassword.getText().toString().trim();
+
+        progressDialog.setMessage("Registering user...");
+        progressDialog.show();
+
+        // Inner Class for string request
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, Constants.URL_REGISTER,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressDialog.dismiss();
+
+                        try {
+
+                            JSONObject jsonObject = new JSONObject(response);
+                            Toast.makeText(getApplicationContext(), "The user were registered" +
+                                    " succesfully!!!", Toast.LENGTH_LONG).show();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressDialog.hide();
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("username", username);
+                params.put("email", email);
+                params.put("password", password);
+                return params;
+            }
+        };
+
+        RequestHandler.getInstance(this).addToRequestQueue(stringRequest);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonRegister)
+            registerUser();
+        if(view == textViewLogin)
+            startActivity(new Intent(this, LoginActivity.class));
+    }
 }
