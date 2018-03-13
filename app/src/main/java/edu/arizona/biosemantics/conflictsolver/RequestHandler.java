944499a1833e9ca8ea0c6672cd9cabc061116fd9
ermangurses <egurses@email.arizona.edu>
2018-03-13 11:48:56 -0700

    RequestHandler Class is added

---
+++ app/src/main/java/edu/arizona/biosemantics/conflictsolver/RequestHandler.java
@@ -0,0 +1,40 @@
+package edu.arizona.biosemantics.conflictsolver;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class RequestHandler {
+
+    private static RequestHandler mInstance;
+    private RequestQueue mRequestQueue;
+    private static Context mCtx;
+
+    private RequestHandler(Context context) {
+        mCtx = context;
+        mRequestQueue = getRequestQueue();
+    }
+
+    public static synchronized RequestHandler getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new RequestHandler(context);
+        }
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+
+            mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+}
