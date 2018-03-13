304b357633fcd1de6b3392b4a06c8ced3ad2d8b2
ermangurses <egurses@email.arizona.edu>
2018-03-13 11:44:00 -0700

    small changes on constants

--- app/src/main/java/edu/arizona/biosemantics/conflictsolver/Constants.java
+++ app/src/main/java/edu/arizona/biosemantics/conflictsolver/Constants.java
@@ -3,8 +3,8 @@ package edu.arizona.biosemantics.conflictsolver;
 
 public class Constants {
 
-    private static final String BASE_URL     = "http://192.168.1.141/sandbox/Android/dev/";
-    public static final  String URL_REGISTER = BASE_URL + "register.php";
-    public static final  String URL_LOGIN    = BASE_URL + "login.php";
+    private static final  String BASE_URL     = "http://192.168.1.141/sandbox/Android/dev/";
+    public  static final  String URL_REGISTER = BASE_URL + "register.php";
+    public  static final  String URL_LOGIN    = BASE_URL + "login.php";
 
 }
