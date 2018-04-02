d15d814a069eeb220fe77d1591f6c3ea3cbb59e6
ermangurses <egurses@email.arizona.edu>
2018-04-01 18:54:46 -0700

    changing the constant names

--- app/src/main/java/edu/arizona/biosemantics/conflictsolver/Constants.java
+++ app/src/main/java/edu/arizona/biosemantics/conflictsolver/Constants.java
@@ -4,7 +4,7 @@ package edu.arizona.biosemantics.conflictsolver;
 public class Constants {
 
     private static final  String BASE_URL     = "http://192.168.1.141/sandbox/ontology-conflict-solver-server/dev/";
-    public  static final  String URL_REGISTER = BASE_URL + "RegisterExpert.php";
-    public  static final  String URL_LOGIN    = BASE_URL + "LoginExpert.php";
+    public  static final  String URL_REGISTER = BASE_URL + "Register/RegisterExpert.php";
+    public  static final  String URL_LOGIN    = BASE_URL + "Login/LoginExpert.php";
 
 }
