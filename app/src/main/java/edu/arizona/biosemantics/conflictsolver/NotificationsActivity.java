ef2fc20c6387cd03243b0e6fc479ccb4079c5b1c
ermangurses <egurses@email.arizona.edu>
2018-05-13 22:55:49 -0700

    button rows are added programmatically

--- app/src/main/java/edu/arizona/biosemantics/conflictsolver/NotificationsActivity.java
+++ app/src/main/java/edu/arizona/biosemantics/conflictsolver/NotificationsActivity.java
@@ -7,6 +7,10 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -37,6 +41,54 @@ public class NotificationsActivity extends AppCompatActivity {
         menuItem.setChecked(true);
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+        RelativeLayout relativeLayoutXML =(RelativeLayout)findViewById(R.id.relativeLayoutXML);
+
+
+        ScrollView scrollView = new ScrollView(this);
+        scrollView.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
+
+        ////////////////////////////////////////////////////////////////////
+        ////////Adding linearLayoutProgVertical layout to scrollView////////
+        ////////////////////////////////////////////////////////////////////
+        LinearLayout linearLayoutProgVertical = new LinearLayout(this);
+        linearLayoutProgVertical.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT));
+        linearLayoutProgVertical.setOrientation(LinearLayout.VERTICAL);
+        scrollView.addView(linearLayoutProgVertical);
+
+        int i = 0;
+        for(i = 0; i < 6; ++i) {
+
+            Button button = new Button(this);
+            button.setText(i+ "  You have a conflict!!!");
+            button.setLeft(10);
+            button.setId(i);
+
+            linearLayoutProgVertical.addView(button);
+
+        }
+
+
+
+        Button button = new Button(this);
+        button.setText(" 10 You have a conflict!!!");
+        button.setLeft(10);
+        button.setId(i);
+
+        linearLayoutProgVertical.addView(button, 0);
+
+
+
+        Button button1 = new Button(this);
+        button1.setText(" 11 You have a conflict!!!");
+        button1.setLeft(10);
+        button1.setId(i);
+
+        linearLayoutProgVertical.addView(button1, 0);
+
+        relativeLayoutXML.addView(scrollView);
+
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
