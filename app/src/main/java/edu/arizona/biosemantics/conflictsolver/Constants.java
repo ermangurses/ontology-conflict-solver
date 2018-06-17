d41065c526f02a03f6a012e138a6f9c5f7bc8082
ermangurses <egurses@email.arizona.edu>
2018-06-16 22:37:16 -0500

    Local IP changes regulary!

--- app/src/main/java/edu/arizona/biosemantics/conflictsolver/Constants.java
+++ app/src/main/java/edu/arizona/biosemantics/conflictsolver/Constants.java
@@ -2,9 +2,8 @@ package edu.arizona.biosemantics.conflictsolver;
 
 
 public class Constants {
-
-    private static final  String BASE_URL     = "http://192.168.1.141/sandbox/ontology-conflict-solver-server/dev/";
+    private static final  String BASE_URL     = "http://10.193.164.31/sandbox/ontology-conflict-solver-server/dev/";
     public  static final  String URL_REGISTER = BASE_URL + "Register/RegisterExpert.php";
     public  static final  String URL_LOGIN    = BASE_URL + "Login/LoginExpert.php";
-
+    public  static final  String URL_GETTASKS  = BASE_URL + "DailyOperations/GetTasks.php";
 }
