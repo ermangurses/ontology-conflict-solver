19e711aac39edf619ee844aed204d715fb251aaa
ermangurses <egurses@email.arizona.edu>
2018-11-15 13:11:53 -0700

    The flag is changed for the pending intern of the push notifications

--- app/src/main/java/edu/arizona/biosemantics/conflictsolver/FirebaseMessagingService.java
+++ app/src/main/java/edu/arizona/biosemantics/conflictsolver/FirebaseMessagingService.java
@@ -17,10 +17,10 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
 
     private void showNotification(String message) {
 
-        Intent i = new Intent(this,MainActivity.class);
-        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        Intent intent = new Intent(this, TasksActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
-        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,i,PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.	FLAG_NO_CREATE);
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
                 .setAutoCancel(true)
