7d0abd7140027ee69e346b9e68df1f48b300a4bf
ermangurses <egurses@email.arizona.edu>
2018-11-21 16:17:53 -0700

    Adding URL for GetCount function

--- app/src/main/java/edu/arizona/biosemantics/conflictsolver/Constants.java
+++ app/src/main/java/edu/arizona/biosemantics/conflictsolver/Constants.java
@@ -11,6 +11,5 @@ public class Constants {
     public  static final String URL_AUDIO           = BASE_URL + "DailyOperations/FileUploadApi/Audio/";
     public  static final String URL_REGISTERTOKEN   = BASE_URL + "DailyOperations/RegisterToken.php";
     public  static final String URL_ISREGISTERED    = BASE_URL + "DailyOperations/IsRegistered.php";
-
-
+    public  static final String URL_GETCOUNT        = BASE_URL + "DailyOperations/GetCount.php";
 }
